ac59dbcdba0f1d3b3c0d0a6cf0a0dadea2de59e1
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2026-03-05 16:22:26 +0200

    minor

--- server/index.ts
+++ server/index.ts
@@ -4,29 +4,9 @@ import exampleRouter from "./routes/router"; // Import the example router
 import express, { Response, Request } from "express"; // Import Express and types for Request and Response
 
 const app = express();
-
-const allowedOrigins = [
-  "http://localhost:3080",
-  "https://dev-task-frontend.vercel.app",
-];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["POST"],
-    credentials: true,
-  }),
-);
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
