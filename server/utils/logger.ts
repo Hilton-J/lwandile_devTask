274615b8602ed0f0e3c31461c563dd188a75e974
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2026-03-05 11:04:15 +0200

    Logger changes

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -1,14 +1,18 @@
 import dayjs from "dayjs";
 import logger from "pino";
 
+const isDevelopment = "development";
+
 const log = logger({
   base: {
     pid: false,
   },
   timestamp: () => `,"time":"${dayjs().format("YYYY-MM-DDTHH:mm:ssZ[Z]")}"`,
-  transport: {
-    target: "pino-pretty",
-  },
+  ...(isDevelopment === "development" && {
+    transport: {
+      target: "pino-pretty",
+    },
+  }),
 });
 
 export default log;
